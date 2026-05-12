657c497ca94368a4d29606fe4f88986be4406ed8
isaac.bregulla <isaac.bregulla@stud.uni-goettingen.de>
2026-05-12 20:03:02 +0200

    fix: fix to the navbar jump problem

--- app/footerbar.tsx
+++ app/footerbar.tsx
@@ -3,7 +3,7 @@
 export function FootBar(){
 
     return (
-        <footer className = "text-xs sm:text-sm md:text-md lg:text-lg lg:p-14 md:p-12 sm:p-10 p-8 text-gray-800 dark:text-gray-300">
+        <footer className = "text-xs sm:text-xs md:text-sm lg:text-md lg:p-14 md:p-12 sm:p-10 p-8 text-gray-700 dark:text-gray-400">
                 <p className = "">Created by Isaac Bregulla</p>
                 <p className = "">Personal page, 2026, find the entire project in github: <a href="mailto:isaacbregulla@gmail.com">isaacbregulla@gmail.com</a></p>
         </footer>
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -14,17 +14,21 @@ export function NavBar(){
     })
 
     useEffect( () => {
+       
         const handleScroll = () => {
             let resize = 0;
             switch(true){
-                case size<640: // sm
+                case size<640: // xs 
                     resize = 56;
                     break;
-                case size<768: //md
-                    resize = 82;
+                case size<768: //sm
+                    resize = 64;
+                    break;
+                case size<1024: //sm
+                    resize = 92;
                     break;
                 default:    // lg
-                    resize = 112;
+                    resize = 120;
                     break;
             } 
             setScrolled(window.scrollY>resize)
@@ -43,18 +47,23 @@ export function NavBar(){
     const pclass = "text-xs sm:text-sm md:text-md lg:text-lg lg:p-8 md:p-6 sm:p-4 p-2"
 
     return (
-        <nav className = {`${scrolled ? "relative" : "flex w-full h-auto"}`}>
-            <div className = {`${scrolled ? "fixed absolute top-0 right-0" : "flex flex-col h-auto w-full"}`}>
 
-                {!scrolled && (<div className = {`flex ${scrolled ? "" : "items-center "} flex w-full bg-zinc-500 dark:bg-zinc-700 lg:h-20 md:h-15 sm:h-10 h-8 p-8`}>
-                    <h1 className = {`flex font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ml-6`}>Personal page</h1>
-                </div>)}
-                <div className = {`${scrolled ? "rounded-bl-md" : ""} flex w-full justify-center items-center lg:h-10 md:h-8 sm:h-6 h-6 dark:bg-zinc-600 bg-zinc-400`}>
-                    {pages.map( (page) => (
-                        <Link key = {page.title} href = {page.page} className = {pclass}>{page.title}</Link> )
-                    )}
+        <nav className = {`flex flex-col w-full h-auto"}`}>
+
+            <div className = {`flex items-center w-full bg-zinc-500 dark:bg-zinc-700 lg:h-20 md:h-15 sm:h-10 h-8 p-8`}>
+                <h1 className = {`flex font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ml-6`}>Personal page</h1>
+            </div>
+
+            <div className = {`flex ${scrolled ? "relative" : "w-full h-auto"}`}>
+                <div className = {`${scrolled ? "fixed absolute top-4 right-4" : "flex flex-col h-auto w-full"}`}>
+                    <div className = {`${scrolled ? "rounded-md" : ""} flex w-full justify-center items-center lg:h-10 md:h-8 sm:h-6 h-6 dark:bg-zinc-600 bg-zinc-400`}>
+                        {pages.map( (page) => (
+                            <Link key = {page.title} href = {page.page} className = {pclass}>{page.title}</Link> )
+                        )}
+                    </div>
                 </div>
             </div>
+
         </nav> 
     );
 }
